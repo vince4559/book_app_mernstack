cb5b9d98e5ed4dfd3a4de793a5a88f38423f0801
aigbojevincent6@gmail.com <aigbojevincent6@gmail.com>
2023-08-16 13:57:21 +0100

    build error fixed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,7 +13,7 @@ const HomePage = () => {
           <div className='mt-5'>
             <h1 className='mb-10 text-center'>Unlimiteed Access to 100k+ Books and Resources</h1>
             <p className='text-amber-500'>
-              The more that you read, the more things you will know. The more that you learn, the more places you'll go. Books are a uniquely portable magic. I kept always two books in my pocket, one to read, one to write in.
+              The more that you read, the more things you will know. The more that you learn, the more places you will go. Books are a uniquely portable magic. I kept always two books in my pocket, one to read, one to write in.
             </p>
             <div className='flex items-center justify-center gap-6 mt-14'>
             <button onClick={() => router.push('/login')}  className='btn btn-primary'>
