1cc89088cb0fb3296067990611093b36926fd501
aigbojevincent6@gmail.com <aigbojevincent6@gmail.com>
2023-08-17 13:07:15 +0100

    fixing error

--- frontend/src/app/utils/constant.ts
+++ frontend/src/app/utils/constant.ts
@@ -1,1 +1,4 @@
-export const baseUrl ="https://new-book-storez.onrender.com"
+export const baseUrl ="https://book-app-mernstack-api.vercel.app";
+
+const vercel = 'https://book-app-mernstack-api.vercel.app';
+const render ="https://new-book-storez.onrender.com";
