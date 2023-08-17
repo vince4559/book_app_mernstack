560b2c309ff531100bf4542c9ee688b5cd676984
aigbojevincent6@gmail.com <aigbojevincent6@gmail.com>
2023-08-17 19:46:01 +0100

    Fix: Vercel file

--- backend/Server.js
+++ backend/Server.js
@@ -14,7 +14,7 @@ app.use(cors({
     credentials: true, 
     origin:["http://localhost:3000"],
     method:["POST", "GET", "DELETE"]
-}))
+}));
 app.use(express.json());
 app.use(cookieParser())
 app.use(express.static('public'))
