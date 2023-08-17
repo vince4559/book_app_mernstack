7ee35960f77747ea150a8a94e8382c3937480118
aigbojevincent6@gmail.com <aigbojevincent6@gmail.com>
2023-08-17 13:31:37 +0100

    fixing error

--- frontend/src/app/utils/constant.ts
+++ frontend/src/app/utils/constant.ts
@@ -1,4 +1,6 @@
-export const baseUrl ="https://book-app-mernstack-api.vercel.app";
+export const baseUrl ="https://benevolent-duckanoo-3b9092.netlify.app";
 
 const vercel = 'https://book-app-mernstack-api.vercel.app';
 const render ="https://new-book-storez.onrender.com";
+
+const netlify = "https://benevolent-duckanoo-3b9092.netlify.app"
