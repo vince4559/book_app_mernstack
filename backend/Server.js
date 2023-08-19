22622637472f11367d452da5a2c485ce7a11beaf
aigbojevincent6@gmail.com <aigbojevincent6@gmail.com>
2023-08-19 19:35:53 +0100

    proxy test

--- backend/Server.js
+++ backend/Server.js
@@ -5,25 +5,18 @@ const mongoose = require("mongoose");
 const cookieParser = require('cookie-parser')
 const bookrouter = require("./routes/BookRoute");
 const userRouter = require("./routes/UserRouter")
-// const proxy = require('express-http-proxy');
-const proxy= require('http-proxy-middleware');
+const proxy = require('express-http-proxy');
+
 
 
 const PORT = process.env.PORT || 5000
 
 // middlewares
 const app = express();
-// http-proxy-middleware
-app.use('/proxy',{
-    pathRewrite:{'^/proxy/':'/'},
-    target: 'http://localhost:3000',
-    changeOrigin: true,
-    secure:true
-}   
-);
+
 
 // http-express proxy
-// app.use('/proxy', proxy("http://localhost:3000"))
+app.use('/proxy', proxy("http://localhost:3000"))
 app.use(cors({
     credentials: true, 
     origin:"http://localhost:3000",
